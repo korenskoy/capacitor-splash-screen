6e0e640dd5c773e73f6bea345617d09eb8a0d1cf
Anton Korenskoy <anton@korenskoy.ru>
2023-10-17 16:12:20 +0200

    Adding scale animation when hiding splash screen

--- ios/Plugin/SplashScreen.swift
+++ ios/Plugin/SplashScreen.swift
@@ -148,9 +148,11 @@ import Capacitor
         DispatchQueue.main.async {
             UIView.transition(with: self.viewController.view, duration: TimeInterval(Double(fadeOutDuration) / 1000), options: .curveLinear, animations: {
                 self.viewController.view.alpha = 0
+                self.viewController.view.transform = CGAffineTransform.identity.scaledBy(x: 7, y: 7);
 
                 if self.config.showSpinner {
                     self.spinner.alpha = 0
+                    self.spinner.transform = CGAffineTransform.identity.scaledBy(x: 7, y: 7);
                 }
             }) { (_: Bool) in
                 self.tearDown()
